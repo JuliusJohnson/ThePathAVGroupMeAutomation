4c857f2af1be3d7a59e5847e04a2bd4752e18fc9
Julius Johnson <contact@juliusj.com>
2022-04-02 21:03:13 -0700

    New Item SQL

--- sendEmail.py
+++ sendEmail.py
@@ -8,6 +8,6 @@ def sendMail(preservice,service):
         f"<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr><td align=\"center\"><h1>Order of Service</h1> \n <p>This is an autogenrated email</p> \n <h2>Pre-Service</h2> {preservice} \n <h2>Service</h2> {service} </td></tr></table>"
     ]
 
-    yag.send('lecale3092@nuesond.com', 'subject', contents)
+    yag.send('J3JJamesJ@gmail.com', 'subject', contents)
 
 # # sendEmail("test")
